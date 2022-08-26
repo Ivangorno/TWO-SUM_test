2fd7e2afb657f99c322a34cfc5a1c28e8c0e190b
Ivangorno <111599129+Ivangorno@users.noreply.github.com>
2022-08-26 21:17:36 +0300

    added comment about "static" in int[] twoSum

--- src/main/java/firstTryPacage/tests/Solution.java
+++ src/main/java/firstTryPacage/tests/Solution.java
@@ -14,6 +14,7 @@ import  java.util.HashMap;
 
                   }
        public static int[] twoSum(int[] nums, int target) {
+           //додав тут static для підтягування в main, але в завданні це public int[] (без static)
 
            int[] rightIndexes = new int[2];
 
